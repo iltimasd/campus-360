ea8f97ea985385c5b0a9f051281c344d689599bd
Iltimas Doha <iltimasd@users.noreply.github.com>
2017-02-10 03:12:17 -0500

    support for ngrok

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'); //require express like adding a script source
 var app = express();			  //create instance
 var server = require('http').createServer(app); //inside the HTTP library use th ecreate server function via Express
-var io = require('socket.io')(server); //point io to the same server as express
+var io = require('socket.io')(server);//point io to the same server as express
 var port = process.env.PORT || 8080; //heroku || local
 var allClients="";	//list of all clients
 var numClients=0;
